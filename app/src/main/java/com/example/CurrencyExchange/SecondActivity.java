c56d499b30b51751e88b5a3a3795aaa878d35fe3
Sethi77 <devanshsethi77@gmail.com>
2021-08-13 19:05:06 +0530

    Welcome to my project.

---
+++ app/src/main/java/com/example/CurrencyExchange/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.CurrencyExchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firstapplication.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
